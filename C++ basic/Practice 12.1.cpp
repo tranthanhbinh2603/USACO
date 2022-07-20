9ad6cbd06fd7622847b1b79bdb38492578438f11
Trần Thanh Bình <tranthanhbinh.thcsthanhson@gmail.com>
2022-07-20 11:21:03 +0700

    Commit ngày 20/7/2022

---
+++ C++ basic/Practice 12.1.cpp
@@ -0,0 +1,8 @@
+//https://www.hackerearth.com/practice/data-structures/hash-tables/basics-of-hash-tables/practice-problems/algorithm/xsquare-and-double-strings-1/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	
+}
