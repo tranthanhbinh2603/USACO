ae19bf192e9932e6c3bc86ed87e7e9b8958164eb
Trần Thanh Bình <tranthanhbinh.thcsthanhson@gmail.com>
2022-07-21 17:30:14 +0700

    Commit ngày 21/7/2022 lần 2

--- C++ basic/14.2.cpp
+++ C++ basic/14.2.cpp
@@ -16,20 +16,22 @@ int main()
 		int n, q;
 		cin >> n >> q;
 		int a[n+10];
-		for (int i = 1; i <= n; i++)
+		int forward[n+10];
+		int backward[n+10];
+		forward[0] = backward[n+1] = 0;
+		for (int i = 1; i <= n; ++i)
 			cin >> a[i];
+		for (int i = 1; i <= n; ++i) 
+			forward[i] = __gcd(forward[i-1], a[i]);
+		for (int i = n; i >= 1; --i)
+			backward[i] = __gcd(backward[i+1], a[i]);
 		while(q--)
 		{
 			int l, r;
 			cin >>  l >> r;
-			int gc = 0;
-			for (int i = 1; i <= l-1; i++)
-				gc = __gcd(gc, a[i]);
-			for (int i = r+1; i <= n; i++)
-				gc = __gcd(gc, a[i]);
-			cout << gc << endl;
+			cout << __gcd(forward[l - 1], backward[r + 1]) << "\n";
 		}
 	}
 
-	//O(T * (N + Q * N)) = O (T * N + T * Q * N) = 100^3
+	//O(T * (N + N)) =
 } 
